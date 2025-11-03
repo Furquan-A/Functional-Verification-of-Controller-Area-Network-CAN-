cef2a2664c02d67fea72b616466b38d178095f20
Furquan-A <furquan.741@gmail.com>
2025-11-02 22:33:02 -0800

    started reg_driver

--- UVM testbench/agents/reg_agent/reg_driver.sv
+++ UVM testbench/agents/reg_agent/reg_driver.sv
@@ -1,1 +1,23 @@
+`ifndef REG_DRIVER_SV
+`define REG_DRIVER_SV
+
+`include "uvm_macros.svh";
+import uvm_pkg::*;
+
 class reg_driver extends uvm_component;
+`uvm_component_utils(reg_driver)
+
+virtual can_if vif;
+reg_agent_config m_cfg;
+
+function new(string name = "reg_driver", uvm_component parent);
+super.new(name,parent);
+endfunction 
+
+function void build_phase(uvm_phase phase);
+super.build_phase(phase);
+
+if(!uvm_config_db#(reg_agent_config) :: get(this,"","m_cfg",m_cfg);
+`uvm_fatal("REG_DRIVER", "cannot get the interface from reg_agent_config. Did you set() it ?",UVM_LOW)
+
+
