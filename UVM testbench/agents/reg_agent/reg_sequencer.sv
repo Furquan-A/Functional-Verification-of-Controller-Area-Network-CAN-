bb0e6b64bd19bf23bf688e4812a2a8b3302471d1
Furquan-A <furquan.741@gmail.com>
2025-11-02 18:57:17 -0800

    reg_sequencer

--- UVM testbench/agents/reg_agent/reg_sequencer.sv
+++ UVM testbench/agents/reg_agent/reg_sequencer.sv
@@ -1,1 +1,13 @@
-class reg_sequencer extends uvm_components;
+class reg_sequencer extends uvm_sequencer#(reg_txns);
+`uvm_component_utils(reg_sequencer)
+
+function new(string name = "reg_sequencer", uvm_component parent);
+super.new(name,parent);
+endfunction 
+
+function void build_phase(uvm_phase phase);
+super.build_phase(phase);
+`uvm_info(get_funn_name(), "Inside Build_phase", UVM)
+endfunction 
+
+endclass 
