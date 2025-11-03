9c86930803cdbca155179a71b76f38138e2c6046
Furquan-A <furquan.741@gmail.com>
2025-11-02 20:52:27 -0800

    started reg agent

--- UVM testbench/agents/reg_agent/reg_agent.sv
+++ UVM testbench/agents/reg_agent/reg_agent.sv
@@ -1,1 +1,11 @@
 class reg_agent extends uvm_component;
+`uvm_component_utils(reg_agent);
+
+virtual can_if vif;
+
+reg_driver        rdrvh;
+reg_monitor       rmonh;
+reg_sequencer     rseqrh;
+reg_agent_config  m_cfg;
+
+
