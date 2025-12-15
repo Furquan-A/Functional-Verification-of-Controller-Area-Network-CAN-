1ad1d80c8dd4257ca019b787d8c3dd1935227aeb
Furquan-A <furquan.741@gmail.com>
2025-12-14 22:16:41 -0800

    reg_monitor started

--- UVM testbench/agents/reg_agent/reg_monitor.sv
+++ UVM testbench/agents/reg_agent/reg_monitor.sv
@@ -1,1 +1,33 @@
+`ifder REG_MONITOR_SV
+`defiine REG_MONITOR_SV
+
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+
 class reg_monitor extends uvm_component;
+	`uvm_component_utils(reg_monitor)
+	
+	uvm_analysis_port#(reg_transaction) ap;
+	
+	virtual can_if;
+	reg_agent_config r_cfg;
+	reg_transaction reg_txn;
+	
+	env_config env_cfg;
+	
+	extern function new(string name = "reg_monitor",uvm_component parent);
+	extern void function build_phase(uvm_phase phase);
+	extern void function connect_phase(uvm_phase phase);
+	extern task run_phase(uvm_phase phase);
+	extern task ;
+	
+endclass : reg_monitor
+
+
+function reg_monitor :: new (string name = "reg_monitor", uvm_component parent);
+	super.new(name,parent);
+	ap = new("ap",this);
+endfunction 
+
+
+	
