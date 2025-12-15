59256f23455b4640b4a958223bfac3d7f7459e9d
Furquan-A <furquan.741@gmail.com>
2025-12-14 23:18:40 -0800

    monitoring the monitor

--- UVM testbench/agents/reg_agent/reg_monitor.sv
+++ UVM testbench/agents/reg_agent/reg_monitor.sv
@@ -17,7 +17,6 @@ class reg_monitor extends uvm_component;
 	
 	extern function new(string name = "reg_monitor",uvm_component parent);
 	extern void function build_phase(uvm_phase phase);
-	extern void function connect_phase(uvm_phase phase);
 	extern task run_phase(uvm_phase phase);
 	extern task ;
 	
@@ -40,5 +39,38 @@ function reg_monitor :: build_phase(uvm_phase phase);
 	if(vif == null)
 		`uvm_fatal("REG_MONITOR","vif is null in the reg_monitor")
 endfunction 
+
+task reg_monitor :: run_phase(uvm_phase phase)
+	`uvm_info("REG_MONITOR","RUN_PHASE of the reg_monitor Started")
+	
+	forever 
+		begin 
+			// --- WISHBONE MODE ----
+			`ifdef CAN_WISHBONE_IF
+				@(posedge vif.wb_clk_i);
+				
+				// Detect active cycle 
+				( if (vif.wb_cb.wb_cyc_i && vif.wb_cb.wb_stb_i && vif.wb_cb.wb_ack_o)
+					begin
+						reg_txn = reg_transaction::type_id::create("reg_txn");
+						
+						// write Operation 
+						if(vif.wb_cb.wb_we_i)
+							begin 
+								t.kind = REG_WRITE;
+								t.addr = vif.wb_cb.wb_adr_i;
+								t.wdata = vif.wb_cb.wb_dat_i;
+							end 
+						else 
+							begin 
+								t.kind = REG_READ;
+								t.addr = vif.wb_cb.wb_adr_i;
+								t.rdata = vif.wb_cb.wb_dat_o;
+							end 
+							
+							t.success = 1'b1;
+							ap.write(t);
+					end 
+				`else 
 	
 	
