fce0125c7a4208d7283a3b924dc032fafc8e9657
Furquan-A <furquan.741@gmail.com>
2025-12-15 04:12:07 -0800

    basic write test in smoke

--- UVM testbench/Tests/can_base_test.sv
+++ UVM testbench/Tests/can_base_test.sv
@@ -13,14 +13,20 @@ class reg_smoke_test extends uvm_test;
 	
 	extern function new(string name = "reg_smoke_test", uvm_component parent);
 	extern void function build_phase(uvm_phase phase);
+	extern task run_phase(uvm_phase phase);
+	
 endclass 
 `endif 
 
 function reg_smoke_test :: new (string name  = "reg_smoke_test", uvm_component parent)l
 	super.new(name,parent);
+endfunction 
+
+function reg_smoke_test :: build_phase(uvm_phase phase);
+	super.build_phase(phase);
 	
 	m_env = env :: type_id:: create("m_env",this);
-	cfg = env_config::type_id::create("cfg");
+	cfg = can_env_config::type_id::create("cfg");
 	
 	// only the reg agent is enabled for this smoke test 
 	cfg.has_reg_agent = 1;
@@ -33,3 +39,23 @@ function reg_smoke_test :: new (string name  = "reg_smoke_test", uvm_component p
 	cfg.has_virtual_sequencer = 0;
 	
 	cfg.r_cfg = new[cfg.no_of_reg_agent];
+	cfg.r_cfg[0] = reg_agent_config::type_id::create("r_cfg0");
+	cfg.r_cfg[0].is_active = UVM_ACTIVE;
+	
+	uvm_config_db #(virtual can_if)::get(this,"","vif",cfg.r_cfg[0].vif);
+	
+	// set the config for env 
+	uvm_config_db #(can_env_config)::set(this,"m_env","can_env_config",cfg);
+endfunction 
+
+task reg_smoke_test :: run_phase(uvm_phase phase);
+	super.run_phase(phase);
+	
+	phase.raise_objection(this);
+	
+	`uvm_info("REG_SMOKE_TEST","starting register smoke test ")
+	
+	// ----------------------------------------------------------------------
+	// BASIC WRITE TEST 
+	// ----------------------------------------------------------------------
+	
