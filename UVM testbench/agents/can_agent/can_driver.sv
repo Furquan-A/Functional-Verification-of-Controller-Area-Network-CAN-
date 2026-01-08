55c8e91d0a2e3974ef76e3244ee07b994997ae67
Furquan-A <furquan.741@gmail.com>
2026-01-08 01:04:38 -0800

    added CRC calculation to the driver class

--- UVM testbench/agents/can_agent/can_driver.sv
+++ UVM testbench/agents/can_agent/can_driver.sv
@@ -25,6 +25,10 @@ class can_driver extends uvm_driver #(can_transaction);
 	bit last_tx_bit;
 	int unsigned same_cnt;
 	
+	// CRC 
+	bit crc_en;
+	bit [14:0] crc_reg;
+	localparam bit [14:0] CAN_CRC15_POLY = 15'h4599;
 	
 	// ================================ Constructor ==============================================================
 	function new (string name = "can_driver", uvm_component parent);
@@ -107,7 +111,7 @@ class can_driver extends uvm_driver #(can_transaction);
 		if(stuff_en && (same_cnt == 5))
 			begin 
 				bit stuff_bit = ~last_tx_bit;
-				
+				drive_raw_bit(stuff_bit);
 				// after inserting a stuff bit , reset the stuff counter for the new run 
 				same_cnt = 1;
 				last_tx_bit = lb; // will be updated again below after driving lb
@@ -120,9 +124,36 @@ class can_driver extends uvm_driver #(can_transaction);
 			// but harmless to keep updated)
 			if(lb == last_tx_bit) 
 				same_cnt++;
+			else 
+				same_cnt--;
 			
 	endtask
 	
+	// CRC CALCATION 
+	function void crc15_update(bit b);
+		bit msb;
+		msb = crc_reg[14]^b;
+		crc_reg = {crc_reg[13:0],1'b0};
+		if(msb)
+			crc_reg = crc_reg ^ CAN_CRC15_POLY;
+	endfunction 
+	
+	task crc_start();
+		crc_reg = 15'h0000;
+		crc_en = 1'b1;
+	endtask 
+	
+	task crc_stop();
+		crc_en = 1'b0;
+	endtask 
+	
+	task drive_frame_bit(bit lb);
+		if(crc_en);
+			crc15_update(lb);
+		drive_logical_bit(lb);
+	endtask 
+	
+	// ============================== IDLE ==================================================================
 	// wait for idle recessive for at least intermission bits ( simple ) 
 	task wait_for_idle_bus();
 		// wait until the bus is recessive 
@@ -137,14 +168,7 @@ class can_driver extends uvm_driver #(can_transaction);
 	// ================================================================================================================
 	
 	task send_frame(can_transaction tr);
-		bit [14:0] crc_seq;
-		bit [3:0] dlc_value;
-		
-		// Bring up assumptions: 
-		//Only DATA/REMOTE FRAMES for now 
-		// CRC computation later, we transmit observed / tr.crc_obs if set , otherwise 0. 
-		crc_seq = (tr.crc_obs !== `x) ? tr.crc_obs : 15'h0000;
-		
+
 		wait_for_idle_bus();
 		
 		// ---------------  SOF  ( dominant 0 , Not stuffed )
@@ -152,6 +176,9 @@ class can_driver extends uvm_driver #(can_transaction);
 		stuff_en = 1'b0;
 		drive_raw_bit(1'b0); // dominant 0 always 
 		init_stuffing(1'b0);
+		
+		crc_start();
+		crc15_update(1'b0); // SOF  = 0  "first bit "
 		
 		// enable the bit stuffing for everything upto the CRC sequence 
 		stuff_en = 1'b1;
@@ -164,14 +191,14 @@ class can_driver extends uvm_driver #(can_transaction);
 				// 11 bit ID , MSB first : id[10:0]
 				for ( int i = 10 ; i >= 0 ; i--)
 					begin 
-						drive_logical_bit(tr.id[i]);
+						drive_frame_bit(tr.id[i]);
 					end 
 				
 				// RTR 1 bit . if RTR = 0 --> has DATA FRAME else REMOTE FRAME 
-				drive_logical_bit((tr.f_type == `CAN_DATA_FRAME) ? 1'b1 : 1'b0);
+				drive_frame_bit((tr.f_type == `CAN_REMOTE_FRAME) ? 1'b1 : 1'b0);
 				
 				// IDE must be 0 for STANDARD FRAME type 
-				drive_logical_bit(1'b0);
+				drive_frame_bit(1'b0);
 			end 
 			
 		// EXTENDED FRAME 
@@ -180,20 +207,20 @@ class can_driver extends uvm_driver #(can_transaction);
 				// EXTENDED FRAME has the ID of 29 bits [28:0], CLASSIC CAN 
 				// base id[28:18] then SRR = 1, IDE = 1, ext ID[17:0], then RTR
 				for(int i = 28; i >= 18; i--)
-					drive_logical_bit(tr.id[i]); // 11 bits 
+					drive_frame_bit(tr.id[i]); // 11 bits 
 					
 				// SRR = 1 ( replaces the RTR of the standarf frame. Only on the bus , its not of the ID)
-				drive_logical_bit(1'b1); // always Recessive 
+				drive_frame_bit(1'b1); // always Recessive 
 				
 				// IDE must be 1 for extended Frame type 
-				drive_logical_bit(1'b1);
+				drive_frame_bit(1'b1);
 				
 				// extended ID[17:0] after the upper 11 bits win the arbitration 
 				for(int i = 17; i >= 0; i--)
-					drive_logical_bit(tr.id[i]) // 18 bits 
+					drive_frame_bit(tr.id[i]) // 18 bits 
 					
 				// RTR bit 
-				drive_logical_bit((tr.f_type == `CAN_DATA_FRAME) ? 1'b1 : 1'b0);
+				drive_frame_bit((tr.f_type == `CAN_REMOTE_FRAME) ? 1'b1 : 1'b0);
 				
 			end 
 					
@@ -204,11 +231,11 @@ class can_driver extends uvm_driver #(can_transaction);
 		// and will drive the DLC bits 
 		
 		// r0 reserved bit 
-		drive_logical_bit(1'b0); // r0 = 0 for classic CAN 
+		drive_frame_bit(1'b0); // r0 = 0 for classic CAN 
 		
 		// DLC [3:0] MSB first
 		for(int i = 3; i >= 0 ; i--)
-			drive_logical_bit(tr.dlc[i]);
+			drive_frame_bit(tr.dlc[i]);
 			
 		// --------------- DATA 
 		if(tr.f_type == `CAN_DATA_FRAME) 
@@ -217,17 +244,18 @@ class can_driver extends uvm_driver #(can_transaction);
 				if(nbytes > 8) 
 					nbytes = 8; // setting the max limit 
 				
-				for ( int bi = 0 ; bi <= nbytes; bi++)
+				for ( int bi = 0 ; bi < nbytes; bi++)
 					begin 
-						byte unsigned db = tr.data.sie();
 						for(int b = 7; b >=0 ; b--)
-							drive_logical_bit(db[b]);
+							drive_frame_bit([bi][b]);
 					end 
 			end 
 			
 		// --------------- CRC sequence and Delimeter 
+		crc_stop();
+		
 		for(int i = 14 ; i>=0 ;i--) 
-				drive_logical_bit(crc_seq[i]);
+				drive_frame_bit(crc_reg[i]);
 			
 			// Stuffing DISABLE after the CRC sequence 
 			stuff_en = 0;
