a3a26790522e02e198bf2f2e42b87241cb013660
Furquan-A <furquan.741@gmail.com>
2026-01-08 19:51:25 -0800

    wrote a simple can test

--- UVM testbench/agents/can_agent/can_tx_seq.sv
+++ UVM testbench/agents/can_agent/can_tx_seq.sv
@@ -21,3 +21,24 @@ class can_tx_seq extends uvm_sequence #(can_transactions);
 		
 		// ---------- Simple directed frame ---------------------
 		
+		tr.can_fmt = `CAN_ID_STD;
+		tr.id = 11'h123;
+		tr.f_type = `CAN_DATA_FRAME;
+		tr.dlc = 4;
+		tr.data = new[4];
+		tr.data[0] = 8'hAA;
+		tr.data[1] = 8'h55;
+		tr.data[2] = 8'h0F;
+		tr.data[3] = 8'hF0;
+		
+		start_item(tr);
+		finish_item(tr);
+		
+		`uvm_info("CAN_TX_SEQ","Sent one CAN Frame ", UVM_LOW)
+		
+	endtask 
+endclass
+`endif
+
+		
+		
